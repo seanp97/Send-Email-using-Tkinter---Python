48af1628adcce143fd2a2ed576dc9d69a761d5ea
seanp97 <sean.pelser97@gmail.com>
2020-02-04 13:13:01 +0000

    Update index.py
    Fix on send to and simplified

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#This works for Gmail only. Allow less secure apps on Gmail settings
+
 from tkinter import *
 import sys
 import tkinter
@@ -10,22 +12,19 @@ server.starttls()
 email = ""
 password = ""
 message = ""
-
-def getEmail():
-    global email
-    email = Entry.get(userEmailEnt)
-
-def getPassword():
-    global password
-    password = Entry.get(userPasswordEnt)
-
-def getMessage():
-    global message
-    message = Entry.get(messageEnt)
+sendTo = ""
 
 def sendEmail():
+    global email
+    email = Entry.get(userEmailEnt)
+    global password
+    password = Entry.get(userPasswordEnt)
+    global message
+    message = Entry.get(messageEnt)
+    global sendTo
+    sendTo = Entry.get(sendToEnt)
     server.login(email, password)
-    server.sendmail(email, email, message)
+    server.sendmail(email, sendTo, message)
 
 app = Tk()
 app.title("Send Email")
@@ -35,22 +34,16 @@ userEmail = Label(app, text="Email", padx=5, pady=5)
 userEmail.grid(column=0, row=0)
 userEmailEnt = Entry(app, bd=5)
 userEmailEnt.grid(column=1, row=0)
-userEmailBtn = Button(app, text="Submit", command=getEmail)
-userEmailBtn.grid(column=2, row=0)
 
 userPassword = Label(app, text="Password", padx=5, pady=5)
 userPassword.grid(column=0, row=1)
 userPasswordEnt = Entry(app, bd=5, show="*")
 userPasswordEnt.grid(column=1, row=1)
-userPasswordBtn = Button(app, text="Submit", command=getPassword)
-userPasswordBtn.grid(column=2, row=1)
 
 messageLabel = Label(app, text="Message", padx=5, pady=5)
 messageLabel.grid(column=0, row=2)
 messageEnt = Entry(app, bd=5)
 messageEnt.grid(column=1, row=2)
-messageBtn = Button(app, text="Submit", command=getMessage)
-messageBtn.grid(column=2, row=2)
 
 sendToLabel = Label(app, text="Send to", padx=5, pady=5)
 sendToLabel.grid(column=0, row=3)
